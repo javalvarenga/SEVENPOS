21e0b91517e4633866bac23f87bee74ce17501d1
kevin <kovallel1@miumg.edu.gt>
2024-09-03 22:34:13 -0600

    SalesController1

---
+++ API/app/Controllers/SalesController.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'Models/Sales.php';
+
+class CustomerController {
+    public function getAll() {
+        $customerModel = new Customer();
+        $customers = $customerModel->getAll();
+        
+        header('Content-Type: application/json');
+        echo json_encode($customers);
+    }
+
+    public function getById($id) {
+        $customerModel = new Customer();
+        $customer = $customerModel->getById($id);
+        
+        header('Content-Type: application/json');
+        echo json_encode($customer);
+    }
+}
