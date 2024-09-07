c585393d21658d396793fab4ed069d446aac4f9a
javalvarenga <abraalvare@gmail.com>
2024-09-06 20:40:31 -0600

    estructura de carpetas front

---
+++ Frontend/screens/sales.php
@@ -0,0 +1,15 @@
+<?php
+// Sales.php
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sales</title>
+  
+</head>
+<body>
+   <h1> SEVENPOS - Sales</h1>
+</body>
+</html>
