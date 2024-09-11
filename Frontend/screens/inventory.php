ae023cde7cb0a0e2dc79353e00368a70a87645a7
JeremyRH26 <jeremyramos150@gmail,com>
2024-09-10 21:50:54 -0600

    Cambios Productos Frontend

--- Frontend/screens/inventory.php
+++ Frontend/screens/inventory.php
@@ -1,11 +1,50 @@
 <?php
-$title = "Inventario - SEVENPOS";
+$title = "INVENTARIO - SEVENPOS";
 
 // Iniciar el buffer de salida para capturar el contenido HTML
 ob_start();
 ?>
 
-<h1> SEVENPOS - Inventario</h1>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventario</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #f4f4f4;
+        }
+    </style>
+</head>
+
+    <h1> SEVENPOS - INVENTARIO</h1>
+    <table id="MyTable">
+        <thead>
+            <tr>
+                <th>ID Producto</th>
+                <th>Nombre</th>
+                <th>Precio Unitario</th>
+                <th>Unidades</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Los datos se llenarán aquí con JavaScript -->
+        </tbody>
+    </table>
+
+    <script type="module" src="../js/products.js"></script>
+
+
 <?php
 // IMPORTANTE PARA USAR EL LAYOUT Asignar el contenido capturado a la variable $content
 $content = ob_get_clean();
