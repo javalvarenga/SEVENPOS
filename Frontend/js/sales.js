aa1a4c425693844aefbb91fd931f60a64221b922
SacalF <frislymanuel07@gmail.com>
2024-09-13 21:51:34 -0600

    Actualizacion

--- Frontend/js/sales.js
+++ Frontend/js/sales.js
@@ -105,7 +105,7 @@ document.getElementById('ventaForm').addEventListener('submit', function(e) {
     })
     .then(response => response.json())
     .then(data => {
-        //document.getElementById('result').innerText = JSON.stringify(data);
+        
         alert("La venta se ha realizado con exito. \nVenta: "+  data.Venta);
     })
     .catch(error => console.error('Error:', error));
